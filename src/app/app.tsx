023fa17601107131c3c8cc4bbef4f73ef19e24f3
Daniel Fry <coderogue@gmail.com>
2024-04-22 16:01:24 -0700

    Converting to Chakra UI components

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,24 @@
 import { AppRoutes } from '@app/routes';
 import { AppState } from '@interfaces/state/appState';
 import { BrowserRouter as Router} from 'react-router-dom'
+import { 
+  ChakraBaseProvider,
+  extendBaseTheme,
+  theme as chakraTheme,
+ } from '@chakra-ui/react'
 import { login } from '@redux/actions';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 
 import '@css/app.css';
+
+const { Button } = chakraTheme.components
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+})
 
 function App() {
   const dispatch = useDispatch();
@@ -20,9 +33,11 @@ function App() {
   }, [dispatch]);
 
   return (
-    <Router>
-      <AppRoutes />
-    </Router>
+    <ChakraBaseProvider theme={theme}>
+      <Router>
+        <AppRoutes />
+      </Router>
+    </ChakraBaseProvider>
   );
 }
 
--- src/app/appFramework.tsx
+++ src/app/appFramework.tsx
@@ -1,9 +1,13 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import logo from '@src/logo.svg';
 import { logout } from '@redux/actions';
 import store from '@redux/store';
 import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+
+import { MdHome, MdLogout } from "react-icons/md";
+import { Button, Container, IconButton, Link } from '@chakra-ui/react'
+import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons'
 
 import '@css/app.css';
 import '@css/main-content.css';
@@ -15,6 +19,14 @@ function AppFramework() {
     const navigate = useNavigate();
     const location = useLocation();
 
+    const navigateHome = () => {
+        navigate("/");
+    };
+
+    const navigateHome2 = () => {
+        navigate("/home");
+    };
+
     const toggleSidebar = () => {
       setCollapsed(!collapsed);
     };
@@ -23,33 +35,76 @@ function AppFramework() {
         localStorage.setItem('urlHistory', location.pathname);
         store.dispatch(logout());
         navigate("/login");
-    }
-
+    };
+    
     return (
         <>
         <div className="App">
             {/* Top bar */}
-            <div id="topbar" className="topbar">
+            <Container className="topbar">
                 <div><img src={logo} className="topbar-logo" alt="logo" /></div>
-                <div>Fantasy Hound</div>
-                <div className="topbar-sections"><button onClick={toggleSidebar}>Toggle Sidebar</button></div>
-                <div className="topbar-sections"><button onClick={userLogOut}>Log Out</button></div>
-            </div>
+                <div className="topbar-app-label">Fantasy Hound</div>
+                <Container className="topbar-button-container">
+                    <IconButton className="topbar-button"
+                        variant='outline'
+                        colorScheme='lime'
+                        fontSize='20px'
+                        aria-label='Log Out' 
+                        icon={<MdLogout />} 
+                        onClick={userLogOut}
+                        size='sm' 
+                    />
+                </Container>
+            </Container>
 
-            {/* Sidebar */}
-            <div id="sidebar" className={`sidebar ${collapsed ? 'collapsed' : ''}`}>
-                <nav>
-                    <ul>
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to='/home'>Home 2</Link></li>
-                    </ul>
-                </nav>
-            </div>
-
-            {/* Main content */}
-            <div id="main-app" className="main-content">
-                <Outlet />
-            </div>
+            <Container display='flex'>
+                {/* Sidebar */}
+                <Container className="sidebar" 
+                    width={!collapsed ? '200px' : '60px'}
+                >
+                    <Container className="sidebar-button">
+                        <IconButton 
+                            variant='outline'
+                            colorScheme='lime'
+                            aria-label='Toggle Sidebar' 
+                            icon={!collapsed ? <ArrowLeftIcon /> : <ArrowRightIcon />}
+                            onClick={toggleSidebar} 
+                            size='sm'
+                        />
+                    </Container>
+                    <Container>
+                            <nav>
+                                <ul>
+                                    <li>
+                                        <Button 
+                                            aria-label='Home' 
+                                            color='lime'
+                                            colorScheme='lime'
+                                            leftIcon={<MdHome />}
+                                            onClick={navigateHome}
+                                            iconSpacing='4px'
+                                        >{!collapsed ? 'Home' : ''}</Button>
+                                    </li>
+                                    <li>
+                                        <Button 
+                                            aria-label='Home' 
+                                            color='lime'
+                                            colorScheme='lime'
+                                            leftIcon={<MdHome />}
+                                            onClick={navigateHome2}
+                                            iconSpacing='4px'
+                                        >{!collapsed ? 'Home 2' : ''}</Button>
+                                    </li>
+                                </ul>
+                            </nav>
+                    </Container>
+                </Container>
+                
+                {/* Main content */}
+                <Container className="main-content">
+                    <Outlet />
+                </Container>
+            </Container>
         </div>
         </>
     );
